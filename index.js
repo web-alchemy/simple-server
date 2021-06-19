ca9b31672cc6b021e899504d04f0d505d607a71b
monochromer <monochromer@mail.ru>
2021-06-19 20:53:45 +0500

    Remove async iterator

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 const { Server, STATUS_CODES } = require('http');
-const { on, EventEmitter } = require('events');
+const { EventEmitter } = require('events');
 const { AsyncLocalStorage } = require('async_hooks');
 
 const asyncLocalStorage = new AsyncLocalStorage();
-
-class WebServer extends Server {
-  [Symbol.asyncIterator]() {
-    return on(this, 'request');
-  }
-}
 
 class Application extends EventEmitter {
   constructor(options) {
@@ -16,7 +10,7 @@ class Application extends EventEmitter {
       captureRejections: true,
       ...options
     });
-    this._server = new WebServer();
+    this._server = new Server();
   }
 
   [Symbol.for('nodejs.rejection')](error) {
@@ -41,35 +35,35 @@ class Application extends EventEmitter {
     }
   }
 
-  async listen(...args) {
-    this._server.listen(...args);
-
-    await Promise.resolve();
-
-    for await (const [req, res] of this._server) {
-      const context = { req, res };
-      asyncLocalStorage.run(context, () => {
-        try {
-          const { method, url } = req;
-          const eventName = method.toUpperCase() + ' ' + url;
-          if (this.hasListeners(eventName)) {
-            this.emit(eventName, context);
-          } else {
-            // TODO: handle via `app.on(Application.NOT_FOUND, (ctx) => {})`?
-            const notFoundError = new Error('Not Found');
-            notFoundError.name = 'NotFound';
-            notFoundError.status = notFoundError.statusCode = 404;
-            throw notFoundError;
-          }
-        } catch (error) {
-          this.handleError(context, error);
+  onRequest = (req, res) => {
+    const context = { req, res };
+    asyncLocalStorage.run(context, () => {
+      try {
+        const { method, url } = req;
+        const eventName = method.toUpperCase() + ' ' + url;
+        if (this.hasListeners(eventName)) {
+          this.emit(eventName, context);
+        } else {
+          // TODO: handle via `app.on(Application.NOT_FOUND, (ctx) => {})`?
+          const notFoundError = new Error('Not Found');
+          notFoundError.name = 'NotFound';
+          notFoundError.status = notFoundError.statusCode = 404;
+          throw notFoundError;
         }
-      })
-    }
+      } catch (error) {
+        this.handleError(context, error);
+      }
+    });
+  }
+
+  async listen(...args) {
+    this._server.on('request', this.onRequest);
+    this._server.listen(...args);
   }
 
   close(callback) {
     return new Promise((resolve, reject) => {
+      this._server.off('request', this.onRequest);
       this._server.close((error) => {
         if (error) {
           reject(error);
